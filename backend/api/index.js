195c217de049c7e5b78e1f42f955f1e10a3e5589
Muhammad Abdullah <91030208+muhabdullahd@users.noreply.github.com>
2024-12-28 12:52:54 -0600

    backend fix again 3

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,6 +27,7 @@
 // // Start the server
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
